6c6eb5f6c4224906cf13920d6449e719e9475d3e
ercrescent <hilalcoding@gmail.com>
2024-08-27 13:53:58 +0300

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Apple on 8.08.2024.
+//
+
+import UIKit
+
+
+import Foundation
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var myCollectionView:UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        myCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        if let flowLayout =  myCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .horizontal
+            
+        }
+    }
+    func getMoviesFromApi() async {
+        let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "language", value: "en-US"),
+        ]
+        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
+        
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGM4ZjEyMTdhOTA1N2Q4OTg2Yjg4NjEyYTljYTUzYiIsIm5iZiI6MTcyNDc0NDc4Ny42MzA1OTcsInN1YiI6IjY2Y2Q4MzVjMzUxYWU1ZmMxMDlkYWUwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.habsWl7sOE3CPwgDugmMUEpcKVrX1YzT36vu2b8FVh0"
+        ]
+        
+        do {
+                let (data, _) = try await URLSession.shared.data(for: request)
+                
+                print(String(decoding: data, as: UTF8.self))
+            } catch {
+                print("error")
+            }
+        }
+}
+
+
+
+extension ViewController:UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellIdentifier", for: indexPath) as! MovieCollectionViewCell
+        cell.setup(movie: movieList[indexPath.row])
+        print(String(indexPath.row) + "Movie Number's title:" + movieList[indexPath.row].title )
+        return cell
+    }
+}
+
+extension ViewController:UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300.0, height: 300.00)
+    }
+}
+
+extension ViewController:UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("SELECTED MOVIE:" + movieList[indexPath.row].title )
+        
+        if let myNavController = self.navigationController {
+            let storyBoard = UIStoryboard(name:"Main", bundle: nil)
+            var detailViewController:DetailViewController = storyBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+            
+            detailViewController.selectedIndex = indexPath.row
+            myNavController.pushViewController(detailViewController , animated: true)
+        }
+     
+    }
+    
+    
+    
+}
+
